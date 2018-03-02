84337649448afa4042e346d4e71a88d01cf564e4
Joseph Martell <jm@minibig.io>
2018-03-02 16:57:20 +0100

    - Advanced design that shows the activities - Bugs fixed - /!\ Distant launcher isn't finished yet

--- activities.php
+++ activities.php
@@ -27,119 +27,125 @@ use Toml\Parser;
 
     <div class="container">
         <?php
-            $conf = Parser::fromFile('config.toml');
+        $conf = Parser::fromFile('config.toml');
 
-            $elasticHost = $conf['elasticsearch']['host'];
-            $elasticJson = null;
-            if (file_exists('http://'.$elasticHost)) {
-                $elasticJsonContent = file_get_contents('http://' . $elasticHost);
-                $elasticJson = json_decode($elasticJsonContent);
-            }
+        // Elasticsearch infos
+        //region Elastic
 
-            $elasticJsonHealth = null;
-            if (file_exists('http://'.$elasticHost.'/_cluster/health')) {
-                $elasticJsonContent = file_get_contents('http://'.$elasticHost.'/_cluster/health');
-                $elasticJsonHealth = json_decode($elasticJsonContent);
-            }
+        $elasticHost = $conf['elasticsearch']['host'];
+        $elasticJson = null;
+        if ($fContent = file_get_contents('http://' . $elasticHost)) {
+            $elasticJsonContent = $fContent;
+            $elasticJson = json_decode($elasticJsonContent);
+        }
 
-            $elasticVersion = '';
-            $elasticClusterName = '';
-            $elasticStatus = '';
-            if ($elasticJson != null && !empty($elasticJson) && $elasticJsonHealth != null && !empty($elasticJsonHealth)) {
-                $elasticVersion = $elasticJson->version->number;
-                $elasticClusterName = $elasticJson->cluster_name;
-                $elasticStatus = $elasticJsonHealth->status;
-            }
+        $elasticJsonHealth = null;
+        if ($fContent = file_get_contents('http://'.$elasticHost.'/_cluster/health')) {
+            $elasticJsonContent = $fContent;
+            $elasticJsonHealth = json_decode($elasticJsonContent);
+        }
 
-            var_dump($elasticJson);
+        $elasticVersion = '';
+        $elasticClusterName = '';
+        $elasticStatus = '';
+        if ($elasticJson != null && !empty($elasticJson) && $elasticJsonHealth != null && !empty($elasticJsonHealth)) {
+            $elasticVersion = $elasticJson->version->number;
+            $elasticClusterName = $elasticJson->cluster_name;
+            $elasticStatus = $elasticJsonHealth->status;
+        }
 
+        //endregion
 
-            $kafkaHosts = $conf['kafka']['hosts'];
-            $kafkaAllGood = true;
-            $kafkaHostsHealth = [];
-            foreach ($kafkaHosts as $kHost) {
-                $host = explode(':', $kHost)[0];
-                $port = explode(':', $kHost)[1];
-                exec('netstat -an | grep '.$host.'.'.$port.' | grep LISTEN', $out);
-                var_dump($out);
-                if (!empty($out)) {
+        // Kafka infos
+        //region Kafka
+        $kafkaHosts = $conf['kafka']['hosts'];
+        $kafkaAllGood = true;
+        $kafkaAllBad = true;
+        $kafkaHostsHealth = [];
+        foreach ($kafkaHosts as $kHost) {
+            $out = null;
+            $host = explode(':', $kHost)[0];
+            $port = explode(':', $kHost)[1];
 
-                }
-            }
+            exec('netstat -an | grep '.$host.'.'.$port.' | grep LISTEN', $out);
 
-            $pdo = new PDO($conf["database"]["connection"]);
+            $kafkaHostsHealth[] = [$kHost => (!empty($out))];
+            if (empty($out))
+                $kafkaAllGood = false;
+            if (!empty($out))
+                $kafkaAllBad = false;
+        }
+        //endregion
 
-            $logstashInstancesSql= $pdo->prepare('select * from logstash_instances');
-            $logstashInstancesSql->execute();
-            $logstashInstances = $logstashInstancesSql->fetchAll();
-            echo '<hr>';
-            foreach ($logstashInstances as $lInstance) {
-                $out = null;
-                var_dump($lInstance);
-                exec('ps -A | grep '.$lInstance['pid'], $out);
-                var_dump($out);
-            }
+        // Logstash instances infos
+        //region Logstash
+        $pdo = new PDO($conf["database"]["connection"]);
 
-            if ($conf['launcher']['distant_launcher'] == true) {
-                $out = null;
-                $hostDistant = $conf['launcher']['distant']['host'];
-                exec('ping '.$hostDistant, $out);
-                var_dump($out);
+        $logstashInstancesSql= $pdo->prepare('select * from logstash_instances');
+        $logstashInstancesSql->execute();
+        $logstashInstances = $logstashInstancesSql->fetchAll();
 
-                $out = null;
-                exec('ssh '.$conf["launcher"]["distant"]["user"].'@'.$conf["launcher"]["distant"]["host"].'
-                             "[[ -f '.$conf["launcher"]["distant"]["path"].'start_logstash.sh ]] && echo "Found" || echo "Not found"', $out);
-                if ($out == 'Found')
-                    echo 'Found !!!';
-                var_dump($out);
+        $logstashInstancesHealth = [];
+        $logstashInstancesAllGood = true;
+        $logstashInstancesAllBad = true;
+        foreach ($logstashInstances as $lInstance) {
+            $out = null;
 
+            exec('ps -elf | grep '.$lInstance['pid'], $out);
+
+            $logstashInstancesHealth[] = ['name' => $lInstance['name'], 'pid' => $lInstance['pid'], 'status' => (sizeof($out) == 2)];
+            if (sizeof($out) == 2) {
+                $logstashInstancesAllBad = false;
             }
             else {
-                $out = null;
-                exec('[[ -f '.$conf["launcher"]["local"]["path"].'start_logstash.sh ]] && echo "Found" || echo "Not found"', $out);
-
-                if ($out[0] == 'Found')
-                    echo 'Found !!!';
-                else
-                    echo 'ko';
+                $logstashInstancesAllGood = false;
             }
+        }
+        if (sizeof($logstashInstances) == 0) {
+            $logstashInstancesAllGood = true;
+            $logstashInstancesAllBad = false;
+        }
+        //endregion
+
+        // Launchers
+        //region launchers
+        $launcherPath = null;
+        $launcherUser = null;
+        $launcherHost = null;
+
+        $launcherFound = false;
+        if ($conf['launcher']['distant_launcher'] == true) {
+            // @TODO : Find on a distant server
+            $out = null;
+            $launcherUser = $conf['launcher']['distant']['user'];
+            $launcherHost = $conf['launcher']['distant']['host'];
+            $launcherPath = $conf['launcher']['distant']['path'];
+
+            exec('ping '.$launcherHost, $out);
+            var_dump($out);
+
+            $out = null;
+            exec('ssh '.$conf["launcher"]["distant"]["user"].'@'.$conf["launcher"]["distant"]["host"].'
+                         "[[ -f '.$conf["launcher"]["distant"]["path"].'start_logstash.sh ]] && echo "Found" || echo "Not found"', $out);
+
+            $launcherFound = ($out[0] == 'Found');
+            var_dump($out);
+
+        }
+        else {
+            $out = null;
+            exec('[[ -f '.$conf["launcher"]["local"]["path"].'start_logstash.sh ]] && echo "Found" || echo "Not found"', $out);
+
+            $launcherPath = $conf["launcher"]["local"]["path"];
+            $launcherFound = ($out[0] == 'Found');
+        }
+        //endregion
         ?>
 
 
 
-
-            <div class="row">
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel">
-                        <div class="panel-heading text-white <?php echo (in_array($elasticStatus, ['yellow', 'green'])) ? 'bg-success':'bg-danger'; ?>">
-                            <div class="text-center">Elasticsearch</div>
-
-                            <div class="text-center check"><i class="material-icons"><?php echo (in_array($elasticStatus, ['yellow', 'green'])) ? 'check_circle':'error'; ?></i></div>
-                        </div>
-                        <a href="#">
-                            <div class="panel-footer">
-                                <span class="pull-left">View Details</span>
-
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel">
-                        <div class="panel-heading text-white bg-info">
-                            <p>Status: <?php echo $elasticStatus; ?><br>
-                               Host: <?php echo $elasticHost; ?><br>
-                               Version: <?php echo $elasticVersion; ?></p>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- /.row -->
-
-        <div class="row">
+        <!-- Elastic -->
+        <div class="row status">
             <div class="col-lg-3 col-md-6">
                 <div class="panel">
                     <div class="panel-heading text-white <?php echo (in_array($elasticStatus, ['yellow', 'green'])) ? 'bg-success':'bg-danger'; ?>">
@@ -147,71 +153,191 @@ use Toml\Parser;
 
                         <div class="text-center check"><i class="material-icons"><?php echo (in_array($elasticStatus, ['yellow', 'green'])) ? 'check_circle':'error'; ?></i></div>
                     </div>
-                    <a href="#">
+                    <!--<a href="#">
                         <div class="panel-footer">
                             <span class="pull-left">View Details</span>
 
                             <div class="clearfix"></div>
                         </div>
-                    </a>
+                    </a>-->
                 </div>
             </div>
 
             <div class="col-lg-3 col-md-6">
                 <div class="panel">
                     <div class="panel-heading text-white bg-info">
-                        <p>Status: <?php echo $elasticStatus; ?><br>
-                            Host: <?php echo $elasticHost; ?><br>
-                            Version: <?php echo $elasticVersion; ?></p>
+                        <p>Status: <?php echo ucfirst($elasticStatus); ?><br>
+                           Host: <?php echo $elasticHost; ?><br>
+                           Version: <?php echo $elasticVersion; ?></p>
 
                     </div>
                 </div>
             </div>
         </div>
         <!-- /.row -->
-        <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-yellow">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-shopping-cart fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge">124</div>
-                                </div>
+
+        <!-- Kafka -->
+        <div class="row status">
+            <div class="col-lg-3 col-md-6">
+                <div class="panel">
+                    <div class="panel-heading text-white <?php
+                    if ($kafkaAllGood)
+                        echo 'bg-success';
+                    elseif ($kafkaAllBad)
+                        echo 'bg-danger';
+                    else
+                        echo 'bg-warning';
+                    ?>">
+                        <div class="text-center">Kafka</div>
+
+                        <div class="text-center check"><i class="material-icons">
+                                <?php
+                                if ($kafkaAllGood)
+                                    echo 'check_circle';
+                                elseif ($kafkaAllBad)
+                                    echo 'error';
+                                else
+                                    echo 'warning';
+                                ?></i></div>
+                    </div>
+                    <!--<a href="#">
+                        <div class="panel-footer">
+                            <span class="pull-left">View Details</span>
+
+                            <div class="clearfix"></div>
+                        </div>
+                    </a>-->
+                </div>
+            </div>
+
+            <?php
+            for ($i=0; $i<sizeof($kafkaHostsHealth); $i++) {
+                foreach ($kafkaHostsHealth[$i] as $host => $health) {
+                    ?>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel">
+                            <div class="panel-heading text-white bg-info">
+                                <p>Host: <?php echo $host; ?><br>
+                                    Status: <?php echo ($health) ? 'On':'Off'; ?></p>
                             </div>
                         </div>
-                        <a href="#">
-                            <div class="panel-footer">
-                                <span class="pull-left">View Details</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
                     </div>
+                    <?php
+                }
+            }
+            ?>
+        </div>
+        <!-- /.row -->
+
+        <!-- Logstash -->
+        <div class="row status">
+            <div class="col-lg-3 col-md-6">
+                <div class="panel">
+                    <div class="panel-heading text-white <?php
+                    if ($logstashInstancesAllGood)
+                        echo 'bg-success';
+                    elseif ($logstashInstancesAllBad)
+                        echo 'bg-danger';
+                    else
+                        echo 'bg-warning';
+                    ?>">
+                        <div class="text-center">Logstash instances</div>
+
+                        <div class="text-center check"><i class="material-icons">
+                                <?php
+                                if ($logstashInstancesAllGood)
+                                    echo 'check_circle';
+                                elseif ($logstashInstancesAllBad)
+                                    echo 'error';
+                                else
+                                    echo 'warning';
+                                ?></i></div>
+                    </div>
+                    <!--<a href="#">
+                        <div class="panel-footer">
+                            <span class="pull-left">View Details</span>
+
+                            <div class="clearfix"></div>
+                        </div>
+                    </a>-->
                 </div>
+            </div>
+
+            <?php
+            for ($i=0; $i<sizeof($logstashInstancesHealth); $i++) {
+                ?>
                 <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-red">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-support fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge">13</div>
-                                </div>
-                            </div>
+                    <div class="panel">
+                        <div class="panel-heading text-white bg-info">
+                            <p>Instance name: <?php echo $logstashInstancesHealth[$i]['name']; ?><br>
+                                Instance PID: <?php echo $logstashInstancesHealth[$i]['pid']; ?><br>
+                                Status: <?php echo ($logstashInstancesHealth[$i]['status']) ? 'Active':'Killed'; ?></p>
                         </div>
-                        <a href="#">
-                            <div class="panel-footer">
-                                <span class="pull-left">View Details</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
                     </div>
                 </div>
-            </div> <!-- /container -->
+                <?php
+            }
+            ?>
+        </div>
+        <!-- /.row -->
+
+        <!-- Launchers -->
+        <div class="row status">
+            <div class="col-lg-3 col-md-6">
+                <div class="panel">
+                    <div class="panel-heading text-white <?php
+                    if ($launcherPath != null && $launcherFound)
+                        echo 'bg-success';
+                    else
+                        echo 'bg-danger';
+                    ?>">
+                        <div class="text-center">Launcher file</div>
+
+                        <div class="text-center check"><i class="material-icons">
+                                <?php
+                                if ($launcherPath != null && $launcherFound)
+                                    echo 'check_circle';
+                                else
+                                    echo 'error';
+                                ?></i></div>
+                    </div>
+                    <!--<a href="#">
+                        <div class="panel-footer">
+                            <span class="pull-left">View Details</span>
+
+                            <div class="clearfix"></div>
+                        </div>
+                    </a>-->
+                </div>
+            </div>
+
+
+            <div class="col-lg-3 col-md-6">
+                <div class="panel">
+                    <div class="panel-heading text-white bg-info">
+                        <?php
+                        if (!$conf['launcher']['distant_launcher']) {
+                            ?>
+                            <p>Path: <span class="small"><?php echo $launcherPath; ?></span><br>
+                                Status: <?php echo ($launcherFound) ? 'Found' : 'Not found'; ?></p>
+                            <?php
+                        }
+                        else {
+                            ?>
+                            <p>Host: <?php echo $launcherUser.'@'.$launcherHost; ?><br>
+                                Path: <span class="small"><?php echo $launcherPath; ?></span><br>
+                                Status: <?php echo ($launcherFound) ? 'Found' : 'Not found'; ?></p>
+                            <?php
+                        }
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
 </main>
 
 <?php
